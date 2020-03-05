d073ae93ab72eee46d8b1ba33478cf87c43423b1
lvhaitao <37372782+lvhaitao0706@users.noreply.github.com>
2020-03-05 21:15:38 +0800

    Update main.c
    定时器使用

--- User/main.c
+++ User/main.c
@@ -3,67 +3,49 @@
 #include  <includes.h>
 
 
-//任务优先级
+
 #define START_TASK_PRIO		3
-//任务堆栈大小	
 #define START_STK_SIZE 		512
-//任务控制块
 OS_TCB StartTaskTCB;
-//任务堆栈	
 CPU_STK START_TASK_STK[START_STK_SIZE];
-//任务函数
 void start_task(void *p_arg);
 
 
 
 
-//任务优先级
-#define LED1_TASK_PRIO		4
-//任务堆栈大小	
+
+#define LED1_TASK_PRIO		5
 #define LED1_STK_SIZE 		128
-//任务控制块
-OS_TCB Led1TaskTCB;
-//任务堆栈	
+OS_TCB Led1TaskTCB;	
 CPU_STK LED1_TASK_STK[LED1_STK_SIZE];
 void led1_task(void *p_arg);
 
 
-//任务优先级
-#define LED2_TASK_PRIO		5
-//任务堆栈大小	
-#define LED2_STK_SIZE 		128
-//任务控制块
-OS_TCB Led2TaskTCB;
-//任务堆栈	
-CPU_STK LED2_TASK_STK[LED2_STK_SIZE];
-void led2_task(void *p_arg);
 
 
 
-/*******************************************************************************
-* 函 数 名         : main
-* 函数功能		   : 主函数
-* 输    入         : 无
-* 输    出         : 无
-*******************************************************************************/
+OS_TMR OsTmr;
+void tim_callback(void *p_arg);
+
+
 int main()
 {
 	OS_ERR  err;
 	OSInit(&err);                                               /* Init uC/OS-III.                                      */
 
-	OSTaskCreate((OS_TCB 	* )&StartTaskTCB,		//任务控制块
-				 (CPU_CHAR	* )"start task", 		//任务名字
-                 (OS_TASK_PTR )start_task, 			//任务函数
-                 (void		* )0,					//传递给任务函数的参数
-                 (OS_PRIO	  )START_TASK_PRIO,     //任务优先级
-                 (CPU_STK   * )&START_TASK_STK[0],	//任务堆栈基地址
-                 (CPU_STK_SIZE)START_STK_SIZE/10,	//任务堆栈深度限位
-                 (CPU_STK_SIZE)START_STK_SIZE,		//任务堆栈大小
-                 (OS_MSG_QTY  )0,					//任务内部消息队列能够接收的最大消息数目,为0时禁止接收消息
-                 (OS_TICK	  )0,					//当使能时间片轮转时的时间片长度，为0时为默认长度，
-                 (void   	* )0,					//用户补充的存储区
-                 (OS_OPT      )OS_OPT_TASK_STK_CHK|OS_OPT_TASK_STK_CLR, //任务选项
-                 (OS_ERR 	* )&err);				//存放该函数错误时的返回值 
+	OSTaskCreate((OS_TCB 	* )&StartTaskTCB,		
+				 (CPU_CHAR	* )"start task", 		
+                 (OS_TASK_PTR )start_task, 			
+                 (void		* )0,					
+                 (OS_PRIO	  )START_TASK_PRIO,     
+                 (CPU_STK   * )&START_TASK_STK[0],	
+                 (CPU_STK_SIZE)START_STK_SIZE/10,	
+                 (CPU_STK_SIZE)START_STK_SIZE,		
+                 (OS_MSG_QTY  )0,					
+                 (OS_TICK	  )0,					
+                 (void   	* )0,				
+                 (OS_OPT      )OS_OPT_TASK_STK_CHK|OS_OPT_TASK_STK_CLR, 
+                 (OS_ERR 	* )&err);		
 
 	OSStart(&err);                                              /* Start multitasking (i.e. give control to uC/OS-III). */
 
@@ -106,8 +88,17 @@ static  void  start_task(void *p_arg)
     App_SerialInit();                                           /* Initialize Serial communication for application ...  */
 #endif
 
-	OS_CRITICAL_ENTER();	//进入临界区
-    //创建LED1任务
+	OSTmrCreate ((OS_TMR  *)&OsTmr,
+										 (CPU_CHAR *)"tmr",
+										 500,
+										 100,
+										 (OS_OPT)OS_OPT_TMR_PERIODIC,
+										 (OS_TMR_CALLBACK_PTR)tim_callback,
+										 (void *)0,
+										 (OS_ERR *)err);
+
+	OS_CRITICAL_ENTER();	
+    
 	OSTaskCreate((OS_TCB 	* )&Led1TaskTCB,		
 				 (CPU_CHAR	* )"led1 task", 		
                  (OS_TASK_PTR )led1_task, 			
@@ -121,48 +112,28 @@ static  void  start_task(void *p_arg)
                  (void   	* )0,					
                  (OS_OPT      )OS_OPT_TASK_STK_CHK|OS_OPT_TASK_STK_CLR,
                  (OS_ERR 	* )&err);		
-	//创建LED2任务
-	OSTaskCreate((OS_TCB 	* )&Led2TaskTCB,		
-				 (CPU_CHAR	* )"led2 task", 		
-                 (OS_TASK_PTR )led2_task, 			
-                 (void		* )0,					
-                 (OS_PRIO	  )LED2_TASK_PRIO,     
-                 (CPU_STK   * )&LED2_TASK_STK[0],	
-                 (CPU_STK_SIZE)LED2_STK_SIZE/10,	
-                 (CPU_STK_SIZE)LED2_STK_SIZE,		
-                 (OS_MSG_QTY  )0,					
-                 (OS_TICK	  )0,					
-                 (void   	* )0,					
-                 (OS_OPT      )OS_OPT_TASK_STK_CHK|OS_OPT_TASK_STK_CLR,
-                 (OS_ERR 	* )&err);
 				 								 
-	OS_TaskSuspend((OS_TCB*)&StartTaskTCB,&err);		//挂起开始任务			 
-	OS_CRITICAL_EXIT();	//进入临界区
+	OS_TaskSuspend((OS_TCB*)&StartTaskTCB,&err);			 
+	OS_CRITICAL_EXIT();	
 }
 
 
 
-//led1任务函数
+
 void led1_task(void *p_arg)
 {
 	OS_ERR err;
 	p_arg = p_arg;
+	OSTmrStart(&OsTmr,&err);
 	while(1)
 	{
 		GPIO_ToggleBits(GPIOF,GPIO_Pin_9);
-		OSTimeDlyHMSM(0,0,0,200,OS_OPT_TIME_HMSM_STRICT,&err); //延时500ms
+		OSTimeDlyHMSM(0,0,0,200,OS_OPT_TIME_HMSM_STRICT,&err);
 	}
 }
 
 
-//led2任务函数
-void led2_task(void *p_arg)
+void tim_callback(void *p_arg)
 {
-	OS_ERR err;
-	p_arg = p_arg;
-	while(1)
-	{
-		GPIO_ToggleBits(GPIOF,GPIO_Pin_10);
-		OSTimeDlyHMSM(0,0,0,500,OS_OPT_TIME_HMSM_STRICT,&err); //延时100ms
-	}
+	GPIO_ToggleBits(GPIOF,GPIO_Pin_10);
 }
